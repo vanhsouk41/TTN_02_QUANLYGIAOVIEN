fa57ad075de2bd35a83eed20dcd0880e8f7ec51e
muoimuoisokiu <muoimuoisokiu@gmail.com>
2019-10-12 19:39:48 +0700

    Update GIAOVIEN.cs
    Them cac thuoc tinh cho giao vien

--- TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/GIAOVIEN.cs
+++ TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/GIAOVIEN.cs
@@ -8,6 +8,17 @@ namespace QUAN_LY_GIAO_VIEN
 {
     class GIAOVIEN
     {
-
+        //Day la form quan ly cac da tinh khac nhau cua giao vien
+        private string name;
+        private string quequan;
+        private string diachi;
+        private string magiaovien;
+        private string sodienthoai;
+        private DateTime ngaysinh;
+        private string email;
+        private string mahocham;
+        private string mahocvi;
+        public GIAOVIEN() { }
+           
     }
 }
