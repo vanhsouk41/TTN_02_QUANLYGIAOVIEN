2b06d043458b1b54c7729e3f070ada276367acf3
vuongnguyen98 <37323991+vuongnguyen98@users.noreply.github.com>
2019-10-15 18:21:25 +0700

    created

---
+++ TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/TaiGV.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace QUAN_LY_GIAO_VIEN
+{
+    partial class TaiGV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+          
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
---
+++ TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/TaiGV.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUAN_LY_GIAO_VIEN
+{
+    public partial class TaiGV : Form
+    {
+        public TaiGV()
+        {
+            InitializeComponent();
+            if(Works_calculating.DT==1)
+                tai_dao_tao();
+            if (Works_calculating.NCKH == 1)
+                show_taiNCKH();
+        }
+
+       
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
