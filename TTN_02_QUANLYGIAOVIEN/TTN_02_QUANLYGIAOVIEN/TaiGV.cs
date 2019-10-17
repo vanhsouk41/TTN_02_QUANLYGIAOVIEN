29bd1cfd5f909f7fcc090185d9c81b4e3b586873
vuongnguyen98 <37323991+vuongnguyen98@users.noreply.github.com>
2019-10-17 18:24:45 +0700

    updated

--- TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/TaiGV.cs
+++ TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/TaiGV.cs
@@ -97,7 +97,80 @@ namespace QUAN_LY_GIAO_VIEN
             dataGridView3.DataSource = myDataSet3.Tables[0];
             dataGridView3.AutoResizeColumns();
         }
-     
+        public void show_taiNCKH()
+        {
+            label1.Text = "Tải bài báo khoa học";
+            label2.Text = "Tải đề tài nghiên cứu";
+            label4.Text = "Tải Sách";
+            string query1 = "SELECT * FROM dbo.taiNCKH_BBKH('Mã giáo viên', @magv,@namhoc)";
+            //Works_calculating w_c = new Works_calculating();
+            string magv = Works_calculating.magvTT;
+            string namhoc = Works_calculating.namhocTT;
+
+            SqlDataAdapter sda = new SqlDataAdapter();
+            DataSet myDataSet = new DataSet();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
+            {
+                connection.Open();
+                SqlCommand sql = connection.CreateCommand();
+                sql.CommandText = query1;
+                sql.Parameters.Add(new SqlParameter("@magv", SqlDbType.NVarChar)).Value = magv;
+                sql.Parameters.Add(new SqlParameter("@namhoc", SqlDbType.NVarChar)).Value = namhoc;
+                sda.SelectCommand = sql;
+                myDataSet = new DataSet();
+                sda.Fill(myDataSet);
+                connection.Close();
+            }
+            dataGridView1.DataSource = myDataSet.Tables[0];
+            dataGridView1.AutoResizeColumns();
+
+            string query2 = "SELECT * FROM dbo.taiNCKH_DTNC('Mã giáo viên',  @magv,@namhoc)";
+            //Works_calculating w_c = new Works_calculating();
+            string magv2 = Works_calculating.magvTT;
+            string namhoc2 = Works_calculating.namhocTT;
+
+            SqlDataAdapter sda2 = new SqlDataAdapter();
+            DataSet myDataSet2 = new DataSet();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
+            {
+                connection.Open();
+                SqlCommand sql2 = connection.CreateCommand();
+                sql2.CommandText = query2;
+                sql2.Parameters.Add(new SqlParameter("@magv", SqlDbType.NVarChar)).Value = magv;
+                sql2.Parameters.Add(new SqlParameter("@namhoc", SqlDbType.NVarChar)).Value = namhoc;
+                sda2.SelectCommand = sql2;
+                myDataSet2 = new DataSet();
+                sda2.Fill(myDataSet2);
+                connection.Close();
+            }
+            dataGridView2.DataSource = myDataSet2.Tables[0];
+            dataGridView2.AutoResizeColumns();
+
+            string query3 = "SELECT * FROM dbo.taiNCKH_SACH('Mã giáo viên', @magv,@namhoc)  ";
+            //Works_calculating w_c = new Works_calculating();
+            string magv3 = Works_calculating.magvTT;
+            string namhoc3 = Works_calculating.namhocTT;
+
+            SqlDataAdapter sda3 = new SqlDataAdapter();
+            DataSet myDataSet3 = new DataSet();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
+            {
+                connection.Open();
+                SqlCommand sql3 = connection.CreateCommand();
+                sql3.CommandText = query3;
+                sql3.Parameters.Add(new SqlParameter("@magv", SqlDbType.NVarChar)).Value = magv;
+                sql3.Parameters.Add(new SqlParameter("@namhoc", SqlDbType.NVarChar)).Value = namhoc;
+                sda3.SelectCommand = sql3;
+                myDataSet3 = new DataSet();
+                sda3.Fill(myDataSet3);
+                connection.Close();
+            }
+            dataGridView3.DataSource = myDataSet3.Tables[0];
+            dataGridView3.AutoResizeColumns();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
