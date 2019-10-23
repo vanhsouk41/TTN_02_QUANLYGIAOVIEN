79dd775bd2f2af48f19500f700fda16e5e963c83
vuongnguyen98 <37323991+vuongnguyen98@users.noreply.github.com>
2019-10-23 18:28:31 +0700

    created

---
+++ TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/Export.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace QUAN_LY_GIAO_VIEN
+{
+    class Export
+    {
+        public static void ExportFile(string Header, DataGridView dgv )
+        {
+           
+        }
+    }
+}
