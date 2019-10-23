aa3126892a8f976f5d99da6a1a583373d9ea9c71
vuongnguyen98 <37323991+vuongnguyen98@users.noreply.github.com>
2019-10-23 18:30:33 +0700

    updated

--- TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/Export.cs
+++ TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/Export.cs
@@ -14,7 +14,58 @@ namespace QUAN_LY_GIAO_VIEN
     {
         public static void ExportFile(string Header, DataGridView dgv )
         {
-           
+            // Tạo đối tượng mở Explorer
+            SaveFileDialog fsave = new SaveFileDialog();
+            // Chỉ ra đuôi của tệp tin
+            fsave.Filter = "(Tất cả các tệp)|*.*|(Các tệp excel)|*.xlsx";
+            fsave.ShowDialog();
+
+            if (fsave.FileName != "")
+            {
+                // Tạo Excel App
+                Excel.Application app = new Excel.Application();
+                // Tạo 1 workbook
+                Excel.Workbook wb = app.Workbooks.Add(Type.Missing);
+                Excel.Worksheet sheet = null;
+                try
+                {
+                    // Đọc dữ liệu
+                    sheet = wb.ActiveSheet;
+                    sheet.Range[sheet.Cells[1, 1], sheet.Cells[1, dgv.ColumnCount]].Merge();
+                    sheet.Cells[1, 1].Value = Header;
+                    sheet.Cells[1, 1].Font.Name = "Times New Roman";
+                    sheet.Cells[1, 1].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                    sheet.Cells[1, 1].Font.Size = 20;
+                    sheet.Cells[1, 1].Borders.Weight = Excel.XlBorderWeight.xlThin;
+                    //Sinh tiêu đề
+                    for (int i = 1, k = 1; i <= dgv.Columns.Count; i++)
+                    {
+                        if (dgv.Columns[i - 1].Visible == false) continue;
+                        sheet.Cells[2, k] = dgv.Columns[i - 1].HeaderText;
+                        sheet.Cells[2, k].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                        sheet.Cells[2, k].Font.Name = "Times New Roman";
+                        sheet.Cells[2, k].Font.Bold = true;
+                        sheet.Cells[2, k].Borders.Weight = Excel.XlBorderWeight.xlThin;
+                        k++;
+                    }
+                    // Sinh dữ liệu
+                   
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    app.Quit();
+                    wb = null;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Bạn không chọn tệp tin nào", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
