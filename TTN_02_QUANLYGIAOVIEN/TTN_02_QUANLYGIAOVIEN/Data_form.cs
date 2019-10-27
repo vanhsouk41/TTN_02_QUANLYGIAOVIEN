59864555f58755da6fae26d348fb8db305bdefd9
vuongnguyen98 <37323991+vuongnguyen98@users.noreply.github.com>
2019-10-27 18:36:40 +0700

    updated

--- TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/Data_form.cs
+++ TTN_02_QUANLYGIAOVIEN/TTN_02_QUANLYGIAOVIEN/Data_form.cs
@@ -32,7 +32,88 @@ namespace QUAN_LY_GIAO_VIEN
             MessageBox.Show("Success!", "Thông báo", MessageBoxButtons.OK);
         }
         // Tìm kiếm bài báo khoa học
-       
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string type = comboBox1.Text;
+            string value = textBox11.Text;
+            HOATDONGNGHIENCUU hdnc = new HOATDONGNGHIENCUU();
+            hdnc.search_BBKH(type, value);
+            dataGridView1.DataSource = hdnc.myDisplayDataTable;
+            dataGridView1.AutoResizeColumns();
+        }
+
+        ///Phát viết
+        ///
+
+        public void load_data_LHP()
+        {
+            string query = "SELECT * FROM  dbo.LOPHOCPHAN";
+            DataSet data = new DataSet();
+
+            using (SqlConnection connect = new SqlConnection(ConnectionString.connectionString))
+            {
+                connect.Open();
+                SqlDataAdapter apter = new SqlDataAdapter(query, connect);
+                apter.Fill(data);
+
+                connect.Close();
+            }
+            panel2.Enabled = false;
+            lop_hp_datagitview.DataSource = data.Tables[0];
+            lop_hp_datagitview.AutoResizeColumns();
+            textBox14_phat.Text = "";
+            textBox18_phat.Text = "";
+            textBox17_phat.Text = "";
+            textBox15_phat.Text = "";
+            textBox19_phat.Text = "";
+            textBox16_phat.Text = "";
+            textBox20_phat.Text = "";
+            textBox21_phat.Text = "";
+            textBox22_phat.Text = "";
+        }
+        private void lop_hp_datagitview_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //string malop, tenlop, siso, loiahp, loaihinhdaotao, ngonngu, namhoc, hocy, mamh;
+            panel2.Enabled = false;
+            string malop = (lop_hp_datagitview.SelectedCells[0].OwningRow.Cells["MaLop"].Value.ToString());
+            string tenlop = (lop_hp_datagitview.SelectedCells[0].OwningRow.Cells["TenLop"].Value.ToString());
+            string siso = (lop_hp_datagitview.SelectedCells[0].OwningRow.Cells["SiSo"].Value.ToString());
+            string loaihp = (lop_hp_datagitview.SelectedCells[0].OwningRow.Cells["LoaiHP"].Value.ToString());
+            string ngonngu = (lop_hp_datagitview.SelectedCells[0].OwningRow.Cells["NgonNgu"].Value.ToString());
+            string loaihinhdaotao = (lop_hp_datagitview.SelectedCells[0].OwningRow.Cells["LoaiHinh_DaoTao"].Value.ToString());
+            string namhoc = (lop_hp_datagitview.SelectedCells[0].OwningRow.Cells["NamHoc"].Value.ToString());
+            string hocky = (lop_hp_datagitview.SelectedCells[0].OwningRow.Cells["HocKy"].Value.ToString());
+            string mamh = (lop_hp_datagitview.SelectedCells[0].OwningRow.Cells["MaMH"].Value.ToString());
+            textBox14_phat.Text = malop;
+            textBox18_phat.Text = tenlop;
+            textBox17_phat.Text = siso;
+            textBox15_phat.Text = loaihp;
+            textBox19_phat.Text = ngonngu;
+            textBox16_phat.Text = loaihinhdaotao;
+            textBox20_phat.Text = namhoc;
+            textBox21_phat.Text = hocky;
+            textBox22_phat.Text = mamh;
+            panel2.Enabled = false;
+
+        }
+        ///Phát viết
+        private void button15_phat_Click(object sender, EventArgs e)
+        {
+            textBox14_phat.Text = "";
+            textBox18_phat.Text = "";
+            textBox17_phat.Text = "";
+            textBox15_phat.Text = "";
+            textBox19_phat.Text = "";
+            textBox16_phat.Text = "";
+            textBox20_phat.Text = "";
+            textBox21_phat.Text = "";
+            textBox22_phat.Text = "";
+            panel2.Enabled = true;
+            Data_form.them = 1;
+            Data_form.sua = 0;
+            Data_form.xoa = 0;
+            panel2.Enabled = true;
+        }
         ///Phát viết
         private void button12_phat_Click(object sender, EventArgs e)
         {
